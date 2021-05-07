4aafe46d2f5ac2a268feb6ac9345aeafa3706a61
colepnc <cole@pointandclicktulsa.com>
2021-05-07 10:51:08 -0500

    20.04 LTS Update
    Update for 20.04. Also included HTTPS in case DNS over HTTPS is ever added for on-the-LAN DNS.

--- pi-hole.sh
+++ pi-hole.sh
@@ -1,26 +1,18 @@
-# Set your static IP information in the variables below
-ip="192.168.1.3"
-netmask="255.255.255.0"
-gateway="192.168.1.1"
-dns="192.168.1.2"
 # Start the script proper
 # Update Ubuntu before starting pi-hole install, and install cron-apt/htop for maintenance purposes
-apt-get update
-apt-get dist-upgrade -y
-apt-get install cron-apt htop linux-virtual-lts-xenial linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial php-zip -y
-# Set static IP, this may need changed if using ipv6
-sed -i -e 's/iface eth0 inet dhcp/#iface eth0 inet dhcp/g' /etc/network/interfaces
-echo "iface eth0 inet static" >> /etc/network/interfaces
-echo "address $ip" >> /etc/network/interfaces
-echo "netmask $netmask" >> /etc/network/interfaces
-echo "gateway $gateway" >> /etc/network/interfaces
-echo "dns-nameservers $dns" >> /etc/network/interfaces
+apt update
+apt dist-upgrade -y
+apt install cron-apt htop php-zip -y
+# Disable ipv6 on all interfaces, remove if your network is actually using ipv6
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/&ipv6.disable=1/' /etc/default/grub
+update-grub
 # Firewall config
 ufw --force enable
 ufw default deny incoming
 ufw default allow outgoing
 ufw allow ssh/tcp
 ufw allow http/tcp
+ufw allow https/tcp
 ufw allow 53/tcp
 ufw allow 53/udp
 reboot
